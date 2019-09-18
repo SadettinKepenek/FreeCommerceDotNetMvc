c455a2c3f9290280aca61bb420e397d88cb1e295
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-18 23:22:27 +0300

    ProductAttributeManager Added, ProductManager added, ProductOptionsManager added, ProductPriceManager added

--- FreeCommerceDotNet/Models/DbManager/ProductAttributeManager.cs
+++ FreeCommerceDotNet/Models/DbManager/ProductAttributeManager.cs
@@ -1,7 +1,80 @@
-﻿namespace FreeCommerceDotNet.Models.DbManager
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+using FreeCommerceDotNet.Models.Util;
+
+namespace FreeCommerceDotNet.Models.DbManager
 {
-    public class ProductAttributeManager
+    public class ProductAttributeManager:IOperations<ProductAttribute>
     {
-        
+        public List<ProductAttribute> GetAll()
+        {
+            string sqlQuery = "select * from ProductsAttributes";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                var Products = new List<ProductAttribute>();
+                Utilities.ExecuteCommand<ProductAttribute>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products;
+            }
+        }
+
+        public ProductAttribute Get(int id)
+        {
+            string sqlQuery = "select * from ProductsAttributes where RelationId=@Id ";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                var Products = new List<ProductAttribute>();
+                Utilities.ExecuteCommand<ProductAttribute>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products.First();
+            }
+        }
+
+        public bool Add(ProductAttribute entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_productattribute_insert"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<ProductAttribute>(sqlCommand, SqlCommandTypes.Insert);
+                return true;
+            }
+        }
+
+        public int Update(ProductAttribute entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_productattribute_update"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<ProductAttribute>(sqlCommand, SqlCommandTypes.Insert);
+                return 0;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            string sqlQuery = "DELETE FROM ProductsAttributes WHERE RelationId=@Id";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                return Utilities.ExecuteCommand<Product>(sqlCommand, SqlCommandTypes.Select);
+            }
+        }
+
+        public bool CheckIsExist(int id)
+        {
+            return Utilities.CheckIsExist("ProductAttributes", "PriceId", id);
+        }
     }
 }
--- FreeCommerceDotNet/Models/DbManager/ProductPriceManager.cs
+++ FreeCommerceDotNet/Models/DbManager/ProductPriceManager.cs
@@ -1,7 +1,81 @@
-﻿namespace FreeCommerceDotNet.Models.DbManager
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+using FreeCommerceDotNet.Models.Util;
+
+namespace FreeCommerceDotNet.Models.DbManager
 {
-    public class ProductPriceManager
+    public class ProductPriceManager:IOperations<ProductPrice>
     {
-        
+
+        public List<ProductPrice> GetAll()
+        {
+            string sqlQuery = "select * from ProductsPrices";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                var Products = new List<ProductPrice>();
+                Utilities.ExecuteCommand<ProductPrice>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products;
+            }
+        }
+
+        public ProductPrice Get(int id)
+        {
+            string sqlQuery = "select * from ProductsPrices where PriceId=@Id ";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                var Products = new List<ProductPrice>();
+                Utilities.ExecuteCommand<ProductPrice>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products.First();
+            }
+        }
+
+        public bool Add(ProductPrice entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_productprice_insert"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<ProductPrice>(sqlCommand, SqlCommandTypes.Insert);
+                return true;
+            }
+        }
+
+        public int Update(ProductPrice entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_productprice_update"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<ProductPrice>(sqlCommand, SqlCommandTypes.Insert);
+                return 0;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            string sqlQuery = "DELETE FROM ProductsPrices WHERE PriceId=@Id";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                return Utilities.ExecuteCommand<Product>(sqlCommand, SqlCommandTypes.Select);
+            }
+        }
+
+        public bool CheckIsExist(int id)
+        {
+            return Utilities.CheckIsExist("ProductPrices", "PriceId", id);
+        }
     }
 }
