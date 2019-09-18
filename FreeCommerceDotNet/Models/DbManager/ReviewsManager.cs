3d89b8f87f5ede4fbac09fe9542b000692e2c8a3
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-18 23:45:47 +0300

    Review Manager Added

--- FreeCommerceDotNet/Models/DbManager/ReviewsManager.cs
+++ FreeCommerceDotNet/Models/DbManager/ReviewsManager.cs
@@ -1,7 +1,80 @@
-﻿namespace FreeCommerceDotNet.Models.DbManager
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+using FreeCommerceDotNet.Models.Util;
+
+namespace FreeCommerceDotNet.Models.DbManager
 {
-    public class ReviewsManager
+    public class ReviewsManager:IOperations<Reviews>
     {
-        
+        public List<Reviews> GetAll()
+        {
+            string sqlQuery = "select * from Reviews";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                var Products = new List<Reviews>();
+                Utilities.ExecuteCommand<Reviews>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products;
+            }
+        }
+
+        public Reviews Get(int id)
+        {
+            string sqlQuery = "select * from Reviews where ReviewId=@Id ";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                var Products = new List<Reviews>();
+                Utilities.ExecuteCommand<Reviews>(sqlCommand, SqlCommandTypes.Select, ref Products);
+                return Products.First();
+            }
+        }
+
+        public bool Add(Reviews entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_review_insert"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<Reviews>(sqlCommand, SqlCommandTypes.Insert);
+                return true;
+            }
+        }
+
+        public int Update(Reviews entry)
+        {
+            using (SqlCommand command = new SqlCommand("sp_review_update"))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand = Utilities.CreateUpdateSqlParameters(sqlCommand, entry, entry.GetType().GetProperties());
+                Utilities.ExecuteCommand<Reviews>(sqlCommand, SqlCommandTypes.Insert);
+                return 0;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            string sqlQuery = "DELETE FROM Reviews WHERE ReviewId=@Id";
+            using (SqlCommand command = new SqlCommand(sqlQuery))
+            {
+                var sqlCommand = command;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                return Utilities.ExecuteCommand<Reviews>(sqlCommand, SqlCommandTypes.Select);
+            }
+        }
+
+        public bool CheckIsExist(int id)
+        {
+            return Utilities.CheckIsExist("Reviews", "ReviewId", id);
+        }
     }
 }
