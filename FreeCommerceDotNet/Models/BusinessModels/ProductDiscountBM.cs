36483d0a3cafa8fc02d163ec7ab705d8bd77b734
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-19 23:36:33 +0300

    ProductAttribuıte ProductDiscount ProductImage

--- FreeCommerceDotNet/Models/BusinessModels/ProductDiscountBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/ProductDiscountBM.cs
@@ -1,7 +1,27 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class ProductDiscountBM
     {
-        
+        public ProductDiscount ProductDiscount { get; set; }
+        public ProductBM ProductBm { get; set; }
+
+        public ProductDiscountBM(int? id)
+        {
+            if (id != null)
+            {
+                using (ProductDiscountManager m = new ProductDiscountManager())
+                {
+                    ProductDiscount = m.Get((int) id);
+                    ProductBm=new ProductBM(ProductDiscount.ProductId);
+                }
+                return;
+            }
+            ProductDiscount=new ProductDiscount();
+            ProductBm=new ProductBM(null);
+
+        }
     }
 }
--- FreeCommerceDotNet/Models/BusinessModels/ProductImageBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/ProductImageBM.cs
@@ -1,7 +1,27 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class ProductImageBM
     {
-        
+        public ProductImage ProductImage { get; set; }
+        public ProductBM ProductBm { get; set; }
+
+        public ProductImageBM(int? relationId)
+        {
+            if (relationId != null)
+            {
+                using (ProductImageManager m = new ProductImageManager())
+                {
+                    ProductImage = m.Get((int) relationId);
+                    ProductBm=new ProductBM(ProductImage.ProductId);
+                }
+                return;
+
+            }
+            ProductImage=new ProductImage();
+            ProductBm=new ProductBM(null);
+        }
     }
 }
