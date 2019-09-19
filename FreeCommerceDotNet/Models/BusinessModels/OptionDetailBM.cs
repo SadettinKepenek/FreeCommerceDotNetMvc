632d67c8545db5972ad0debb40227e3846199d51
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-19 23:15:29 +0300

    OptionDetails and OptionMaster added

--- FreeCommerceDotNet/Models/BusinessModels/OptionDetailBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/OptionDetailBM.cs
@@ -1,7 +1,36 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class OptionDetailBM
     {
-        
+        public OptionDetail OptionDetail { get; set; }
+        public List<OptionMaster> OptionMasters { get; set; }
+
+        public OptionDetailBM(int? id)
+        {
+            if (id == null)
+            {
+                OptionDetail = new OptionDetail();
+                OptionMasters = new List<OptionMaster>();
+            }
+            else
+            {
+
+                using (var m = new OptionDetailManager())
+                {
+                    int key = (int)id;
+                    OptionDetail = m.Get(key);
+                }
+
+                using (var m = new OptionMasterManager())
+                {
+                    int key = (int)id;
+                    OptionMasters = m.GetByIntegerKey(key, "OrdersMaster", "OrderId");
+                }
+            }
+        }
     }
 }
--- FreeCommerceDotNet/Models/BusinessModels/OptionMasterBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/OptionMasterBM.cs
@@ -1,7 +1,40 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class OptionMasterBM
     {
-        
+        public OptionMaster OptionMaster  { get; set; }
+        public List<Customer> Customers { get; set; }
+        public List<Payment> Payments { get; set; }
+        public List<Shipping> Shippings { get; set; }
+
+        public OptionMasterBM(int? id)
+        {
+            if (id == null)
+            {
+                OptionMaster = new OptionMaster();
+            }
+            else
+            {
+                using (var m = new OptionMasterManager())
+                {
+                    int key = (int)id;
+                    OptionMaster = m.Get(key);
+                }
+                using (var m = new PaymentsManager())
+                {
+                    int key = (int)id;
+                    Payments = m.GetByIntegerKey(key, "PaymentGateways", "PaymentId");
+                }
+                using (var m = new ShippingManager())
+                {
+                    int key = (int)id;
+                    Shippings = m.GetByIntegerKey(key, "Shippings", "ShippingId");
+                }
+            }
+        }
     }
 }
