fc6a5d49abb8f8a1c53108ae016704359dff76b1
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-19 23:39:56 +0300

    ProductOption and ProductPrices BM's added

--- FreeCommerceDotNet/Models/BusinessModels/ProductOptionBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/ProductOptionBM.cs
@@ -1,7 +1,43 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class ProductOptionBM
     {
-        
+        public ProductOption ProductOption { get; set; }
+        public List<OptionDetail> OptionDetails { get; set; }
+        public List<Product> Products { get; set; }
+
+        public ProductOptionBM(int? id)
+        {
+            if (id == null)
+            {
+                ProductOption = new ProductOption();
+                OptionDetails = new List<OptionDetail>();
+                Products = new List<Product>();
+            }
+            else
+            {
+
+                using (var m = new ProductOptionsManager())
+                {
+                    int key = (int)id;
+                    ProductOption = m.Get(key);
+                }
+                using (var m = new OptionDetailManager())
+                {
+                    int key = (int)id;
+                    OptionDetails = m.GetByIntegerKey(key, "OptionsDetail", "ValueId");
+                }
+                using (var m = new ProductManager())
+                {
+                    int key = (int)id;
+                    Products = m.GetByIntegerKey(key, "Products", "ProductId");
+                }
+
+            }
+        }
     }
 }
--- FreeCommerceDotNet/Models/BusinessModels/ProductPriceBM.cs
+++ FreeCommerceDotNet/Models/BusinessModels/ProductPriceBM.cs
@@ -1,7 +1,38 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessModels
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
 {
     public class ProductPriceBM
     {
-        
+        public ProductPrice ProductPrice { get; set; }
+        public List<Product> Products { get; set; }
+
+        public ProductPriceBM(int? id)
+        {
+            if (id == null)
+            {
+                ProductPrice = new ProductPrice();
+                Products = new List<Product>();
+
+            }
+            else
+            {
+
+                using (var m = new ProductPriceManager())
+                {
+                    int key = (int)id;
+                    ProductPrice = m.Get(key);
+                }
+
+                using (var m = new ProductManager())
+                {
+                    int key = (int)id;
+                    Products = m.GetByIntegerKey(key, "Products", "ProductId");
+                }
+            }
+        }
+
     }
 }
