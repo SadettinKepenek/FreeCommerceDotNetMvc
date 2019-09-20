759cd0166b89776ef25224c18915cec1dcd76612
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-21 00:40:21 +0300

    OptionDetail and Shipping's BusinessManager's added

--- FreeCommerceDotNet/Models/BusinessManager/OptionDetailBusinessManager.cs
+++ FreeCommerceDotNet/Models/BusinessManager/OptionDetailBusinessManager.cs
@@ -1,7 +1,72 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessManager
+﻿using System;
+using System.Collections.Generic;
+using FreeCommerceDotNet.Models.BusinessModels;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+
+namespace FreeCommerceDotNet.Models.BusinessManager
 {
-    public class OptionDetailBusinessManager
+    public class OptionDetailBusinessManager : IBusinessOperations<OptionDetailBM>
     {
-        
+        public int Add(OptionDetailBM entry)
+        {
+            using (OptionDetailManager manager = new OptionDetailManager())
+            {
+                entry.OptionDetail.OptionId = entry.OptionMaster.OptionId;
+                return manager.Add(entry.OptionDetail);
+
+            }
+        }
+
+        public bool Delete(OptionDetailBM entry)
+        {
+            using (OptionDetailManager manager = new OptionDetailManager())
+            {
+                return manager.Delete(entry.OptionMaster.OptionId);
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public List<OptionDetailBM> Get()
+        {
+            using (OptionDetailManager manager = new OptionDetailManager())
+            {
+                List<OptionDetail> dbOptionDetail = manager.GetAll();
+                List<OptionDetailBM> businessModels = new List<OptionDetailBM>();
+
+                foreach (var details in dbOptionDetail)
+                {
+                    businessModels.Add(new OptionDetailBM(details.OptionId));
+                }
+
+                return businessModels;
+            }
+        }
+
+        public OptionDetailBM GetById(int id)
+        {
+            return new OptionDetailBM(id);
+        }
+
+        public bool Update(OptionDetailBM entry)
+        {
+            try
+            {
+                using (OptionDetailManager manager = new OptionDetailManager())
+                {
+                    manager.Update(entry.OptionDetail);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
--- FreeCommerceDotNet/Models/BusinessManager/ShippingBusinessManager.cs
+++ FreeCommerceDotNet/Models/BusinessManager/ShippingBusinessManager.cs
@@ -1,7 +1,70 @@
-﻿namespace FreeCommerceDotNet.Models.BusinessManager
+﻿using System;
+using System.Collections.Generic;
+using FreeCommerceDotNet.Models.BusinessModels;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+
+namespace FreeCommerceDotNet.Models.BusinessManager
 {
-    public class ShippingBusinessManager
+    public class ShippingBusinessManager : IBusinessOperations<ShippingBM>
     {
-        
+        public int Add(ShippingBM entry)
+        {
+            using (ShippingManager manager = new ShippingManager())
+            {
+                return manager.Add(entry.Shipping);
+            }
+        }
+
+        public bool Delete(ShippingBM entry)
+        {
+            using (ShippingManager manager = new ShippingManager())
+            {
+                return manager.Delete(entry.Shipping.ShippingId);
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public List<ShippingBM> Get()
+        {
+            using (ShippingManager manager = new ShippingManager())
+            {
+                List<Shipping> dbStores = manager.GetAll();
+                List<ShippingBM> businessModels = new List<ShippingBM>();
+
+                foreach (var shipping in dbStores)
+                {
+                    businessModels.Add(new ShippingBM(shipping.ShippingId));
+                }
+
+                return businessModels;
+            }
+        }
+
+        public ShippingBM GetById(int id)
+        {
+            return new ShippingBM(id);
+        }
+
+        public bool Update(ShippingBM entry)
+        {
+            try
+            {
+                using (ShippingManager manager = new ShippingManager())
+                {
+                    manager.Update(entry.Shipping);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
