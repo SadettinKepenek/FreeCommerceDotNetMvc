767bc9c359b10bc45f4a4ffd0328a5d91f931b7f
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-21 00:41:50 +0300

    Logger Implement

---
+++ FreeCommerceDotNet/Models/Util/LogEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FreeCommerceDotNet.Models.Util
+{
+    public class LogEntry
+    {
+        public readonly LogType Severity;
+        public readonly string Message;
+        public readonly string Owner;
+
+        public LogEntry(LogType severity, string message,string owner)
+        {
+            if (message == null) throw new ArgumentNullException("message");
+            if (owner == null) throw new ArgumentNullException("owner");
+            if (message == string.Empty) throw new ArgumentException("empty", "message");
+            if (owner == string.Empty) throw new ArgumentException("empty", "owner");
+            this.Severity = severity;
+            this.Message = message;
+            this.Owner = owner;
+        }
+    
+    }
+}
---
+++ FreeCommerceDotNet/Models/Util/Loggers.cs
@@ -0,0 +1,19 @@
+﻿using FreeCommerceDotNet.Models.Interfaces;
+
+namespace FreeCommerceDotNet.Models.Util
+{
+    public class DatabaseLogger : ILogger
+    {
+        public void Log(LogEntry entry)
+        {
+            LoggerExtensions.Log(this, "Database Logger "+entry.Message, entry.Owner);
+        }
+    }
+    public class EMailLogger : ILogger
+    {
+        public void Log(LogEntry entry)
+        {
+            LoggerExtensions.Log(this, "Email Logger "+entry.Message, entry.Owner);
+        }
+    }
+}
