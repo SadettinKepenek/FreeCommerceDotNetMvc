80d98c1ed8d8fc6502bd4f728d057d428a9918df
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-23 16:37:04 +0300

    UsersBm and manager added in Business Layer

---
+++ FreeCommerceDotNet/Models/BusinessModels/UsersBM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
+{
+    public class UsersBM
+    {
+        public Users Users { get; set; }
+
+        public UsersBM(int? id)
+        {
+            if (id == null)
+            {
+                Users = new Users();
+            }
+            else
+            {
+
+                using (var m = new UsersManager())
+                {
+                    int key = (int)id;
+                    Users = m.Get(key);
+                }
+            }
+        }
+    }
+}
