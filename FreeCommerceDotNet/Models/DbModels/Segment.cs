c072b89ee6eba5bcaf9004111d71034f1da8579c
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-24 16:53:10 +0300

    DbModel and DBManager added for Segment table

---
+++ FreeCommerceDotNet/Models/DbModels/Segment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FreeCommerceDotNet.Models.DbModels
+{
+    public class Segment
+    {
+        public int SegmentId { get; set; }
+        public string SegmentName { get; set; }
+        public string Priorty { get; set; }
+
+    }
+}
