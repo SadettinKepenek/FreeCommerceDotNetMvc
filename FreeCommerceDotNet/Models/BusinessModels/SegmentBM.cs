6381a0835e047c2d0f747fef24db691fcd30944d
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-24 17:10:16 +0300

    Business Manager and Business Model added for Segment Table

---
+++ FreeCommerceDotNet/Models/BusinessModels/SegmentBM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+
+namespace FreeCommerceDotNet.Models.BusinessModels
+{
+    public class SegmentBM
+    {
+        public Segment Segment { get; set; }
+
+        public SegmentBM(int? id)
+        {
+            if (id == null)
+            {
+                Segment = new Segment();
+            }
+            else
+            {
+                using (var m = new SegmentManager())
+                {
+                    int key = (int)id;
+                    Segment = m.Get(key);
+                }
+            }
+        }
+
+    }
+}
