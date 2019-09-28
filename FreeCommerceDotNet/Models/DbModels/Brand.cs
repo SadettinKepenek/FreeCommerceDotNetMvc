2dac19551c7cf9415d20ff3c707620e6df780453
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-28 20:48:45 +0300

    DbModel and DbManager for Brand is done

---
+++ FreeCommerceDotNet/Models/DbModels/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FreeCommerceDotNet.Models.DbModels
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandDescription { get; set; }
+        public string BrandUrl { get; set; }
+        public string BrandImageUrl { get; set; }
+    }
+}
