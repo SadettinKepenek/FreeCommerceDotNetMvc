884ba7dad27e9f107ecf46aff90f128f3d27ca1f
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-28 20:55:53 +0300

    BusinessModel and BusinessManager is done for Brand

---
+++ FreeCommerceDotNet/Models/BusinessManager/BrandBusinessManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FreeCommerceDotNet.Models.BusinessModels;
+using FreeCommerceDotNet.Models.DbManager;
+using FreeCommerceDotNet.Models.DbModels;
+using FreeCommerceDotNet.Models.Interfaces;
+using Microsoft.Ajax.Utilities;
+
+namespace FreeCommerceDotNet.Models.BusinessManager 
+{
+    public class BrandBusinessManager : IBusinessOperations<BrandBM>
+    {
+        public int Add(BrandBM entry)
+        {
+            using (BrandManager manager = new BrandManager())
+            {
+                return manager.Add(entry.Brand);
+            }
+        }
+
+        public bool Delete(BrandBM entry)
+        {
+            using (BrandManager manager = new BrandManager())
+            {
+                return manager.Delete(entry.Brand.BrandId);
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BrandBM> Get()
+        {
+            using (BrandManager manager = new BrandManager())
+            {
+                List<Brand> dbBrands = manager.GetAll();
+                List<BrandBM> businessModel = new List<BrandBM>();
+
+                foreach (var brands in dbBrands)
+                {
+                    businessModel.Add(new BrandBM(brands.BrandId));
+                }
+
+                return businessModel;
+            }
+        }
+
+        public BrandBM GetById(int id)
+        {
+            return new BrandBM(id);
+        }
+
+        public bool Update(BrandBM entry)
+        {
+            try
+            {
+                using (BrandManager manager = new BrandManager())
+                {
+                    manager.Update(entry.Brand);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        }
+    }
+}
