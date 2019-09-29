eef265a898f356f322286bb4d18784c6edf41b76
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-09-29 23:06:57 +0300

    OrderMaster's DbModel updated

--- FreeCommerceDotNet/Models/DbModels/OrderMaster.cs
+++ FreeCommerceDotNet/Models/DbModels/OrderMaster.cs
@@ -6,5 +6,10 @@
         public int PaymentGatewayId { get; set; }
         public int ShippingId { get; set; }
         public int CustomerId { get; set; }
+        public string TrackNumber { get; set; }
+        public string OrderDate { get; set; }
+        public string DeliveryDate { get; set; }
+        public string DeliveryComment { get; set; }
+        public string DeliveryStatus { get; set; }
     }
 }
