a99edb58fd6877abdb6f681db4c545998d586b04
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-30 21:47:34 +0300

    Master

--- FreeCommerceDotNet/Controllers/HomeController.cs
+++ FreeCommerceDotNet/Controllers/HomeController.cs
@@ -11,24 +11,6 @@ namespace FreeCommerceDotNet.Controllers
         {
             return View();
         }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Test()
-        {
-            int a = 3;
-            return View();
-        }
+        // Master
     }
 }
