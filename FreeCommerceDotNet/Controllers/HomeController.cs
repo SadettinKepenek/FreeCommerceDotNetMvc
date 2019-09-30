f1ed8b66a7d3d9cb4222dcbc2cc3140120099fca
berkayalcin <yalcinberkaycom@gmail.com>
2019-09-30 21:29:46 +0300

    removed methods

--- FreeCommerceDotNet/Controllers/HomeController.cs
+++ FreeCommerceDotNet/Controllers/HomeController.cs
@@ -11,19 +11,7 @@ namespace FreeCommerceDotNet.Controllers
         {
             return View();
         }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
         // Develop Branch
 
     }
