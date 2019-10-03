f570ce90651ff0cc03f913cd8e249ddca52f25f1
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-03 21:31:09 +0300

    Add attribute and attribute group entity

---
+++ FreeCommerceDotNet.Entities/Concrete/AttributeGroup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.Entities.Abstract;
+
+namespace FreeCommerceDotNet.Entities.Concrete
+{
+    public class AttributeGroup:IEntity
+    {
+        private int _AttributeGroupId;
+
+        public int AttributeGroupId
+        {
+            get { return _AttributeGroupId; }
+            set { _AttributeGroupId = value; }
+        }
+        private string _AttributeGroupName;
+
+        public string AttributeGroupName
+        {
+            get { return _AttributeGroupName; }
+            set { _AttributeGroupName = value; }
+        }
+        public List<Attribute> Attributes { get; set; }
+
+    }
+}
