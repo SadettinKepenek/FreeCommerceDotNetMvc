598b66207d182334686e8f0843dcd27c905584a6
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-10-04 22:07:39 +0300

    Program.cs modify

--- FreeCommerceDotNet.LayerTest/Program.cs
+++ FreeCommerceDotNet.LayerTest/Program.cs
@@ -14,7 +14,7 @@ namespace FreeCommerceDotNet.LayerTest
         static void Main(string[] args)
         {
             //TestAttributeGroupRepository();
-
+            TestReview();
         }
 
         private static void TestAttributeGroupRepository()
@@ -23,6 +23,22 @@ namespace FreeCommerceDotNet.LayerTest
             var attributeGroup = new AttributeGroup() {AttributeGroupName = "DB Layer Test Group Name", AttributeGroupId = 15};
             DBResult result = repository.Delete(attributeGroup.AttributeGroupId);
             Console.WriteLine("Deleted Entity ID " + result.Id + " Message " + result.Message);
+            Console.ReadKey();
+        }
+
+        private static void TestReview()
+        {
+            IRepository<Reviews> repository = new ReviewRepository();
+            // var reviews = new Reviews() { ReviewId = 4,CustomerId = 14, ProductId = 3, Title = "test1", Text = "reviewUpdateTest", Date = "test", LikeCount = 5, DislikeCount = 0, Rating = 5, Status = true };
+            //  DBResult result = repository.Delete(4);
+            // Console.WriteLine("Updated Entity ID " + result.Id + " Message " + result.Message);
+            foreach (var items in repository.SelectAll())
+            {
+                Console.Write("ReviewId:"+items.ReviewId+"\n");
+                Console.Write("Comment:" + items.Text + "\n");
+                Console.Write("Title:" + items.Title + "\n");
+                Console.Write("Status:" + items.Status + "\n");
+            }
             Console.ReadKey();
         }
     }
