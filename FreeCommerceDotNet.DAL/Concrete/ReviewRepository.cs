894c67b04797ce2307e7960e88238f41388e5914
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-10-04 20:31:58 +0300

    Modify mssql database Modify review repository

--- FreeCommerceDotNet.DAL/Concrete/ReviewRepository.cs
+++ FreeCommerceDotNet.DAL/Concrete/ReviewRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using FreeCommerceDotNet.Common.Concrete;
 using FreeCommerceDotNet.DAL.Abstract;
 using FreeCommerceDotNet.Entities.Concrete;
 
@@ -11,7 +12,7 @@ namespace FreeCommerceDotNet.DAL.Abstract
     public class ReviewRepository:IReviewDal
     {
         string connectionString = "server=94.73.144.8;Database=u8206796_dbF1B;User Id=u8206796_userF1B;Password=SPlt16S0;";
-
+        MsSQLDatabase database = new MsSQLDatabase();
         public DBResult Delete(int id)
         {
             throw new NotImplementedException();
@@ -20,11 +21,11 @@ namespace FreeCommerceDotNet.DAL.Abstract
         public DBResult Insert(Reviews entity)
         {
             string query = "ReviewInsertUpdateDelete";
-            using(SqlConnection connection = new SqlConnection(connectionString))
+            using (var connection=database.CreateConnection())
             {
-                SqlCommand command = new SqlCommand(query,connection);
+                SqlCommand command = new SqlCommand(query, connection);
                 command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@Action","INSERT");
+                command.Parameters.AddWithValue("@Action", "INSERT");
                 command.Parameters.AddWithValue("@CustomerId", entity.CustomerId);
                 command.Parameters.AddWithValue("@ProductId", entity.ProductId);
                 command.Parameters.AddWithValue("@Title", entity.Title);
@@ -34,25 +35,16 @@ namespace FreeCommerceDotNet.DAL.Abstract
                 command.Parameters.AddWithValue("@LikeCount", entity.LikeCount);
                 command.Parameters.AddWithValue("@DislikeCount", entity.DislikeCount);
                 command.Parameters.AddWithValue("@Status", entity.Status);
-                if (connection.State == ConnectionState.Closed)
-                    connection.Open();
-                try
+                DataTable datatable=database.DoQuery(command: command);
+                if (datatable.Rows.Count!=0)
                 {
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        int Id = (int)reader[0];
-                        string message = reader[1] as string;
-                        Debug.WriteLine(message);
-                        return new DBResult() { Id = Id, Message = message };
-                    }
-
-                }
-                catch (Exception e)
-                {
-                    Debug.WriteLine(e.StackTrace);
+                    DBResult result = new DBResult();
+                    result.Id = (int)datatable.Rows[0]["ReturnValue"];
+                    result.Message = datatable.Rows[0]["Message"].ToString();
+                    return result;
                 }
             }
+            
             return null;
         }
 
@@ -69,7 +61,7 @@ namespace FreeCommerceDotNet.DAL.Abstract
         public DBResult Update(Reviews entity)
         {
             string query = "ReviewInsertUpdateDelete";
-
+            return null;
         }
     }
 }
