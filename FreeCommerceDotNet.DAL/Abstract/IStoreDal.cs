bcb4fa9641db3bc66f88d80c619f51da7f3c8bcc
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-10-06 15:01:12 +0300

    Add IStoreDal Add StoreRepository

---
+++ FreeCommerceDotNet.DAL/Abstract/IStoreDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCommerceDotNet.DAL.Abstract
+{
+    public interface IStoreDal : IRepository<Store>
+    {
+
+    }
+}
