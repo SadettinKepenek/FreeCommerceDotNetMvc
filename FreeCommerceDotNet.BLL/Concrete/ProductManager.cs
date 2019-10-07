85492f72b85882408e95c120c394d132afaeb84c
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-07 18:54:48 +0300

    Add IProductService Add ProductManager

---
+++ FreeCommerceDotNet.BLL/Concrete/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using FreeCommerceDotNet.BLL.Abstract;
+using FreeCommerceDotNet.Common.Concrete;
+using FreeCommerceDotNet.DAL.Abstract;
+using FreeCommerceDotNet.Entities.Concrete;
+
+namespace FreeCommerceDotNet.BLL.Concrete
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager()
+        {
+            
+        }
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public ServiceResult Insert(Product entity)
+        {
+            return ServiceHelper.FromDbResultToServiceResult(_productDal.Insert(entity));
+        }
+
+        public ServiceResult Update(Product entity)
+        {
+            return ServiceHelper.FromDbResultToServiceResult(_productDal.Update(entity));
+        }
+
+        public ServiceResult Delete(int id)
+        {
+            return ServiceHelper.FromDbResultToServiceResult(_productDal.Delete(id));
+        }
+
+        public Product SelectById(int id)
+        {
+            return _productDal.SelectById(id);
+        }
+
+        public List<Product> SelectByFilter(List<DBFilter> filters)
+        {
+            return _productDal.SelectByFilter(filters);
+        }
+
+        public List<Product> SelectAll()
+        {
+            return _productDal.SelectAll();
+        }
+    }
+}
