a213cc52cace5606b029f0949841c35812ff673b
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-10 21:20:11 +0300

    Modify Category view for new model

--- FreeCommerceDotNet/Controllers/HomeController.cs
+++ FreeCommerceDotNet/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
-﻿using FreeCommerceDotNet.Models.BusinessManager;
-using FreeCommerceDotNet.Models.BusinessModels;
-using System;
-using System.Diagnostics;
+﻿using FreeCommerceDotNet.BLL.Concrete;
+using FreeCommerceDotNet.DAL.Concrete;
+using FreeCommerceDotNet.Entities.Concrete;
+using FreeCommerceDotNet.Models.BusinessManager;
+using FreeCommerceDotNet.Models.ControllerModels.Client.ClientFilters;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using FreeCommerceDotNet.Models.ControllerModels.Client;
-using System.Collections.Generic;
-using FreeCommerceDotNet.Models.ControllerModels.Client.ClientFilters;
 
 namespace FreeCommerceDotNet.Controllers
 {
@@ -16,29 +15,79 @@ namespace FreeCommerceDotNet.Controllers
         {
             return View();
         }
-        
+
         // Develop Branch
         public ActionResult Product(int id)
         {
-            
+
             using (ProductBusinessManager manager = new ProductBusinessManager())
             {
                 var products = manager.GetById(id);
                 return View(products);
             }
-            
+
 
         }
 
-        public ActionResult Category(int id,int page=0,CategoryFilters filters=null)
+        public ActionResult Category(int id, int page = 0, CategoryFilters filters = null)
         {
-            Category category;
-            category = new Category(id, page,filters);
+            int PRODUCTCOUNT = 12;
+            page = page != 0 ? page : 1;
+
+            CategoryManager manager = new CategoryManager(new CategoryRepository());
+            var category = manager.SelectById(id);
+            IEnumerable<Product> productsList = category.Products;
+
+            #region ApplyPagination
+
+            if (productsList.Count()>PRODUCTCOUNT)
+            {
+                productsList = productsList.Skip(page * PRODUCTCOUNT).Take(PRODUCTCOUNT);
+            }
+
+
+            #endregion
+
+            #region ApplyFilters 
+            if (filters != null)
+            {
+                if (filters.AltKategoriId != 0)
+                {
+                    productsList = productsList.Where(x => x.CategoryId == filters.AltKategoriId);
+                }
+
+                if (filters.BrandId != 0)
+                {
+                    productsList = productsList.Where(x => x.Brand == filters.BrandId);
+                }
+
+            }
+
+            #endregion
+
+            #region ToList
+
+            category.Products = productsList.ToList();
+
+            #endregion
+
+
+            #region FindMaxPage
+
+            var pageCount = (int)category.Products.Count / PRODUCTCOUNT;
+            ViewBag.MaxPage = pageCount == 0 ? 1:pageCount;
             
+            ViewBag.Page = page;
+
+            #endregion
+
+
+
+
             return View(category);
         }
 
-      
-    
+
+
     }
 }
