fcc1690ae4fc36f40b660c2cbac91a6813498aaf
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-13 01:48:38 +0300

    Add Card css items addCardButtons.css Add Card to Account Controller

--- FreeCommerceDotNet/assets/fcdnJs/productPageShoppingCart.js
+++ FreeCommerceDotNet/assets/fcdnJs/productPageShoppingCart.js
@@ -8,21 +8,23 @@ $(document).on('click', '.productPageAddToCard', function () {
 
     var productImageHtml =
         '<a href="#">' +
-            '<img class="img-fluid blur-up lazyload pro-img" src = '+window.productImageUrl+' alt = "" >' +
+        '<img class="img-fluid blur-up lazyload pro-img" src = ' + window.productImageUrl + ' alt = "" >' +
         '</a>';
 
     var productDescriptionSection =
-        '<a href="#"><h6><i class="fa fa-check" ></i>Product<span> '+window.productName+' </span><span> successfully added to your Cart</span></h6 ></a>';
+        '<a href="#"><h6><i class="fa fa-check" ></i>Product<span> ' + window.productName + ' </span><span> successfully added to your Cart</span></h6 ></a>';
 
 
     var cartItem = {
         productId: window.productId,
         productName: window.productName,
-        productCount:window.productCount
+        productCount: window.productCount,
+        productPrice: window.productPrice,
+        productImageUrl: window.productImageUrl
     };
 
     var cartItems = [];
-    cartItems=getCookie('cartListCookie');
+    cartItems = getCookie('cartListCookie');
     if (cartItems == null) {
         cartItems = [];
         cartItems.push(cartItem);
@@ -81,11 +83,141 @@ $(document).on('click', '.quantity-right-plus', function () {
 
 });
 
+$(document).on('input', '.cartPageQuantityInput', function () {
+    var inputElementId = this.id;
+    var productId = inputElementId.replace('productcountbyid_', '');
+    var productNewCount = this.value;
+    console.log(productNewCount === '');
+    if (productNewCount === '') {
+        productNewCount = 0;
+        removeFromCart(productId);
+        location.reload();
+        console.log('Product Id ' + productId + ' Silindi');
+    }
+    else if (productNewCount == 0) {
+        removeFromCart(productId);
+        console.log('Product Id ' + productId + ' Silindi');
+        location.reload();
+    } else {
+        changeProductCount(productId, productNewCount);
+    }
+    console.log('Product Id ' + productId + ' New Count ' + productNewCount);
 
-$('input[name=quantity]').on('input', function() {
+});
+
+$(document).on('click', '.cartPageRemoveProduct', function () {
+    var inputElementId = this.id;
+    var productId = inputElementId.replace('productremovebyid_', '');
+    removeFromCart(productId);
+    console.log('Product Id ' + productId + ' Silindi');
+    location.reload();
+
+});
+
+$('input[name=quantity]').on('input', function () {
     console.log('changed');
 });
 
-var getCartItems = function() {
+var getCartItems = function () {
     return JSON.parse(getCookie("cartListCookie"));
+};
+
+var removeFromCart = function (productId) {
+    var cartItems = getCartItems();
+    const idx = cartItems.findIndex(x => x.productId == productId);
+    if (idx !== undefined) cartItems.splice(idx, 1);
+    setCookie('cartListCookie', JSON.stringify(cartItems), 1);
+    updateCartTotalPrice();
+};
+
+var changeProductCount = function(productId, productNewCount) {
+    var cartItems = getCartItems();
+    const idx = cartItems.findIndex(x => x.productId == productId);
+    if (idx !== undefined) {
+        cartItems[idx].productCount = productNewCount;
+    };
+
+    
+
+    setCookie('cartListCookie', JSON.stringify(cartItems), 1);
+
+    var price = $('#mobileProductPriceById_' + productId).html().replace('₺','').replace(' ','');
+    var totalPrice = price * productNewCount;
+    
+    $('#totalProductPriceById_' + productId).html(totalPrice+' ₺');
+    updateCartTotalPrice();
+
+};
+
+var updateCartTotalPrice = function() {
+
+    var itemsInCart = getCartItems();
+    window.gelenUrunler = itemsInCart;
+
+    var genelToplam = 0;
+
+    for (var i = 0; i < itemsInCart.length; i++) {
+        var item = itemsInCart[i];
+        var productCount = item.productCount;
+        var productPrice = item.productPrice;
+        genelToplam += productPrice * productCount;
+    }
+    $('#totalPriceH2').html(genelToplam + ' ₺');
+
+};
+
+
+var fillProductsInCart = function() {
+    var tbody = $('#cartItemsTbody');
+    var itemsInCart = getCartItems();
+    window.gelenUrunler = itemsInCart;
+
+    var genelToplam = 0;
+
+    for (var i = 0; i < itemsInCart.length; i++) {
+        var item = itemsInCart[i];
+        var productId = item.productId;
+        var productName = item.productName;
+        var productCount = item.productCount;
+        var productImageUrl = item.productImageUrl;
+        var productPrice = item.productPrice;
+
+        var totalPrice = productPrice * productCount;
+        genelToplam += totalPrice;
+        tbody.append(' <tr>' +
+            '<td><a href="#"><img src=' + productImageUrl + ' alt=""></a></td>' +
+            '<td><a href="#">'+productName+'</a>' +
+            '<div class="mobile-cart-content row"><div class="col-xs-3">' +
+            '<div class="qty-box"><div class="input-group">' +
+            '<input type="text" name="quantity" class="form-control input-number" value="'+productCount+'">' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="col-xs-3">' +
+            '<h2 class="td-color" id="mobileProductPriceById_'+productId+'">'+productPrice+'₺</h2>' +
+            '</div>' +
+            '<div class="col-xs-3">' +
+            '<h2 class="td-color">' +
+            '<a href="#" class="icon">' +
+            '<i class="ti-close">' +
+            '</i>' +
+            '</a>' +
+            '</h2>' +
+            '</div>' +
+            '</div>' +
+            '</td>' +
+            '<td><h2 id="ProductPriceById_' + productId +'">' + productPrice +' ₺</h2>' +
+            '</td>' +
+            '<td>' +
+            '<div class="qty-box">' +
+            '<div class="input-group">' +
+            '<input type="number" id="productcountbyid_'+productId+'" name="quantity" class="form-control input-number cartPageQuantityInput" value="'+productCount+'">' +
+            '</div></div></td>' +
+            '<td><h2 class="td-color" id="totalProductPriceById_' + productId +'">' + totalPrice+' ₺</h2></td>' +
+            '<td><a href="#" class="icon cartPageRemoveProduct" id="productremovebyid_'+productId+'"><i class="ti-close"></i></a></td>' +
+            '</tr>');
+    }
+    $('#totalPriceH2').html(totalPrice+' ₺');
+
+
 };
