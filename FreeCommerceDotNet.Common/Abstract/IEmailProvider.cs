782ad76795824b51e7501ba333e3540691cc97bc
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-15 19:21:02 +0300

    Add IEmailProvider

---
+++ FreeCommerceDotNet.Common/Abstract/IEmailProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCommerceDotNet.Common.Abstract
+{
+    public interface IEmailProvider
+    {
+        bool Send(string from, string to, string message);
+        bool Send(string to, string message);
+    }
+}
