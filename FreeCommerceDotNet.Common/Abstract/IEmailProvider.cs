bcf02c9ad3f8670ee4818a20207245b131e5239a
berkayalcin <yalcinberkaycom@gmail.com>
2019-10-15 21:16:02 +0300

    Add IEmailLogger Add IEmailProvider Add ILogger Add OutlookMailManager

--- FreeCommerceDotNet.Common/Abstract/IEmailProvider.cs
+++ FreeCommerceDotNet.Common/Abstract/IEmailProvider.cs
@@ -2,7 +2,7 @@
 {
     public interface IEmailProvider
     {
-        bool Send(string from, string to, string message);
-        bool Send(string to, string message);
+
+        bool Send(string to,string subject, string message);
     }
 }
---
+++ FreeCommerceDotNet.Common/Abstract/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeCommerceDotNet.Common.Abstract
+{
+    public interface ILogger
+    {
+        void Log(string message);
+        void Log(string message, string owner);
+        void Log(string message, string owner,Exception e);
+
+    }
+}
